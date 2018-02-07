eb983f628eeb95bf77ea1299c2c7e5379f15a60a
Jed Reisner <djechotech@gmail.com>
2018-02-07 16:07:31 -0700

    Added an empty password test case. Added test groups.

--- src/main/java/test/Tests.java
+++ src/main/java/test/Tests.java
@@ -8,32 +8,39 @@ import static org.testng.Assert.assertEquals;
 public class Tests extends TestBase {
 
     //Login Page Tests
-    @Test
+    @Test(groups = {"smoke", "login"})
     public void testLoginInvalidBoth(){
         setMobileTest(false);
         LoginPage l = new LoginPage(driver);
         assertEquals(l.loginTest("https://stageearlypay.apio.tech/login/","test@test.com", "test1"), true);
     }
 
-    @Test
+    @Test(groups = {"smoke", "login"})
     public void testLoginInvalidUser(){
         setMobileTest(false);
         LoginPage l = new LoginPage(driver);
         assertEquals(l.loginTest("https://stageearlypay.apio.tech/login/","invalid@test.com", "testing123"), true);
     }
 
-    @Test
+    @Test(groups = {"smoke", "login"})
     public void testLoginInvalidPass(){
         setMobileTest(false);
         LoginPage l = new LoginPage(driver);
         assertEquals(l.loginTest("https://stageearlypay.apio.tech/login/","test@apio.tech", "invalid"), true);
     }
 
-    @Test
+    @Test(groups = {"smoke", "login"})
     public void testLoginValid(){
         setMobileTest(false);
         LoginPage l = new LoginPage(driver);
         assertEquals(l.loginTest("https://stageearlypay.apio.tech/login/","test@apio.tech", "testing123"), false);
+    }
+
+    @Test(groups = {"smoke", "login"})
+    public void testBlankPass(){
+        setMobileTest(false);
+        LoginPage l = new LoginPage(driver);
+        assertEquals(l.loginTest("https://stageearlypay.apio.tech/login/","test@apio.tech", ""), true);
     }
 
 }
