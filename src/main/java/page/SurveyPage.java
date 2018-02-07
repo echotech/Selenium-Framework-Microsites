b2204951ed41dba7c676f8d84636235cd1517301
Jed Reisner <djechotech@gmail.com>
2018-02-07 16:39:29 -0700

    Added survey page.

---
+++ src/main/java/page/SurveyPage.java
@@ -0,0 +1,19 @@
+package page;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import util.Helpers;
+import org.slf4j.*;
+
+import java.util.concurrent.TimeUnit;
+
+public class SurveyPage {
+    private WebDriver driver;
+    private Helpers h;
+    static Logger logger = LoggerFactory.getLogger(LoginPage.class);
+
+
+
+}
+
