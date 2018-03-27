22fe0b0360bb55329c71e03d26ac3a2bb1158aed
Jed Reisner <djechotech@gmail.com>
2018-03-27 15:39:46 -0600

    Changed test annotations.

--- src/main/java/test/Tests.java
+++ src/main/java/test/Tests.java
@@ -11,57 +11,57 @@ import static org.testng.Assert.assertEquals;
 public class Tests extends TestBase {
 
     //Login Page Tests
-    @Test(groups = {"smoke", "login"})
+    @Test
     public void testLoginInvalidBoth(){
         setMobileTest(false);
         LoginPage l = new LoginPage(driver);
         assertEquals(l.loginTest("https://stageearlypay.apio.tech/login/","test@test.com", "test1"), true);
     }
 
-    @Test(groups = {"smoke", "login"})
+    @Test
     public void testLoginInvalidUser(){
         setMobileTest(false);
         LoginPage l = new LoginPage(driver);
         assertEquals(l.loginTest("https://stageearlypay.apio.tech/login/","invalid@test.com", "testing123"), true);
     }
 
-    @Test(groups = {"smoke", "login"})
+    @Test
     public void testLoginInvalidPass(){
         setMobileTest(false);
         LoginPage l = new LoginPage(driver);
         assertEquals(l.loginTest("https://stageearlypay.apio.tech/login/","test@apio.tech", "invalid"), true);
     }
 
-    @Test(groups = {"smoke", "login"})
+    @Test
     public void testLoginValid(){
         setMobileTest(false);
         LoginPage l = new LoginPage(driver);
         assertEquals(l.loginTest("https://stageearlypay.apio.tech/login/","test@apio.tech", "testing123"), false);
     }
 
-    @Test(groups = {"smoke", "login"})
+    @Test
     public void testBlankPass(){
         setMobileTest(false);
         LoginPage l = new LoginPage(driver);
         assertEquals(l.loginTest("https://stageearlypay.apio.tech/login/","test@apio.tech", ""), true);
     }
 
-    @Test(groups = {"smoke", "login"})
+    @Test
     public void testSignup(){
         setMobileTest(false);
         LoginPage l = new LoginPage(driver);
         assertEquals("https://stageearlypay.apio.tech/login/#/new_account", l.signupTest("https://stageearlypay.apio.tech/login/"));
     }
 
-    @Test(groups= {"smoke", "login"})
-    public void forgotPassword(){
-
-    }
-
-
-    @Test(groups = {"smoke", "login"})
-    public void changePassword(){
-
-    }
+//    @Test
+//    public void forgotPassword(){
+//
+//    }
+//
+//
+//    @Test
+//    public void changePassword(){
+//
+//    }
 
 }
